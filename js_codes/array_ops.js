08f75f3c693a542924b0f5dc59c0defc3d7106a4
Ronak Jethwa <rjethwa@rjethwa-mac.local>
2020-04-12 17:08:28 -0400

    prettier

--- js_codes/array_ops.js
+++ js_codes/array_ops.js
@@ -1,19 +1,19 @@
 // merge and sort two arrays
-	let array1 = [ 'bat', 'cat', 'hat', 'mat']
-	let array2 = [ 'chat', 'gnat', 'mat', 'tat']
-	let fullArray = [];
-	let fullArrraySorted = [];
+let array1 = ['bat', 'cat', 'hat', 'mat']
+let array2 = ['chat', 'gnat', 'mat', 'tat']
+let fullArray = [];
+let fullArrraySorted = [];
 
-	function concatMyArrays () {
-		return array1.concat(array2);
-	}
+function concatMyArrays() {
+    return array1.concat(array2);
+}
 
-	function sortMyArray (fullArray) {
-		fullArraySorted = fullArray.sort();	
-	}
+function sortMyArray(fullArray) {
+    fullArraySorted = fullArray.sort();
+}
 
-	fullArray = concatMyArrays();
-	console.log("Merged Array is " + fullArray);
-	
-	sortMyArray(fullArray);
-	console.log("Sorted Array is " + fullArraySorted);
+fullArray = concatMyArrays();
+console.log("Merged Array is " + fullArray);
+
+sortMyArray(fullArray);
+console.log("Sorted Array is " + fullArraySorted);
--- js_codes/arrow_functions.js
+++ js_codes/arrow_functions.js
@@ -1,28 +1,28 @@
 // without arrow function
 function ask(question, yes, no) {
-  if (confirm(question)) yes()
-  else no();
+    if (confirm(question)) yes()
+    else no();
 }
 
 ask(
-  "Do you agree?",
-  function() { alert("You agreed."); },
-  function() { alert("You canceled the execution."); }
+    "Do you agree?",
+    function () { alert("You agreed."); },
+    function () { alert("You canceled the execution."); }
 );
 
 
 // with arrow function
 function ask(question, yes, no) {
-  if (confirm(question)) yes()
-  else no();
+    if (confirm(question)) yes()
+    else no();
 }
 
 ask(
-  "Do you agree?",
-  () => alert("You agreed."),
-  () => alert("You canceled the execution.")
+    "Do you agree?",
+    () => alert("You agreed."),
+    () => alert("You canceled the execution.")
 );
 
 
 // most concise arrow function
-( () => confirm("Do you agree?") ? alert("You agreed") : alert("You canceled execution") )()
+(() => confirm("Do you agree?") ? alert("You agreed") : alert("You canceled execution"))()
