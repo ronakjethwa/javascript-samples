5d004b80ad2ca3b60ebe50462147c384920ef3bb
Ronak Jethwa <rjethwa@rjethwa-mac.local>
2020-04-12 16:22:05 -0400

    docs update

---
+++ js_codes/print_prime.js
@@ -0,0 +1,14 @@
+let primeRequirement = prompt("Prime until when?", "")
+let primeNumbers = [];
+
+nextPrime:
+for ( let i = 2; i <= primeRequirement; i++) {
+	for ( let j = 2; j < i; j++) {
+		if ( i % j == 0 ) {
+			continue nextPrime
+		}
+	}
+	primeNumbers.push(i);
+}
+
+console.log(primeNumbers);
