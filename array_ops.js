a3b1211a038926ad74bbac48a243a8e39785a9ce
Ronak Jethwa <rjethwa@rjethwa-mac.local>
2020-04-12 16:20:16 -0400

    docs update

---
+++ array_ops.js
@@ -0,0 +1,18 @@
+	let array1 = [ 'bat', 'cat', 'hat', 'mat']
+	let array2 = [ 'chat', 'gnat', 'mat', 'tat']
+	let fullArray = [];
+	let fullArrraySorted = [];
+
+	function concatMyArrays () {
+		return array1.concat(array2);
+	}
+
+	function sortMyArray (fullArray) {
+		fullArraySorted = fullArray.sort();	
+	}
+
+	fullArray = concatMyArrays();
+	console.log("Merged Array is " + fullArray);
+	
+	sortMyArray(fullArray);
+	console.log("Sorted Array is " + fullArraySorted);
---
+++ arrow_functions.js
@@ -0,0 +1,31 @@
+// without arrow function
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  function() { alert("You agreed."); },
+  function() { alert("You canceled the execution."); }
+);
+
+
+// with arrow function
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  () => alert("You agreed."),
+  () => alert("You canceled the execution.")
+);
+
+
+// most concised
+
+( () => confirm("Do you agree?") ? alert("You agreed") : alert("You canceled execution") )()
