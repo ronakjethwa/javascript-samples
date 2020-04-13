bea43d183a6de8071e6d72faab7002fd5abcf366
Ronak Jethwa <rjethwa@rjethwa-mac.local>
2020-04-12 20:44:34 -0400

    update-array

--- js-short-codes/array_ops.js
+++ js-short-codes/array_ops.js
@@ -1,3 +1,4 @@
+// Operation 1
 // merge and sort two arrays
 let array1 = ['bat', 'cat', 'hat', 'mat']
 let array2 = ['chat', 'gnat', 'mat', 'tat']
@@ -17,3 +18,21 @@ console.log("Merged Array is " + fullArray);
 
 sortMyArray(fullArray);
 console.log("Sorted Array is " + fullArraySorted);
+
+
+// Operation 2
+// different ways to update an array item using index
+let numbers = ['one', 'two', 'three', 'four'];
+let index = 1;
+
+// Approach 1
+numbers[index] = "new_item";
+// output = ['one', 'new_item', 'three', 'four'];
+
+// Approach 2
+number.splice(replaceIndex, 1, "new_item")
+// output = ['one', 'new_item', 'three', 'four'];
+
+// Approach 2
+number.Object.assign([...numbers], {[index]: "new_item"})
+// output = ['one', 'new_item', 'three', 'four'];
